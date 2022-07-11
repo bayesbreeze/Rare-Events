33c32460faab591794a75dd5434bbd32d761f7e8
stalendp <stalendp@gmail.com>
2022-07-11 13:06:53 +0800

    rare events

--- demos/nde/distributions/__init__.py
+++ demos/nde/distributions/__init__.py
@@ -1,6 +1,8 @@
 from .base import Distribution
 from .base import NoMeanException
 
+from .uniform import TweakedUniform
+
 from .normal import StandardNormal
 from .normal import ConditionalDiagonalNormal
 
